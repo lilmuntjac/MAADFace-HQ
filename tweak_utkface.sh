1a08011a6718dec96bf2f23c86cb30c4fb46edbb
lilmuntjac <recapseason@gmail.com>
2023-09-17 15:02:43 -0400

    new format

--- tweak_utkface.sh
+++ tweak_utkface.sh
@@ -3,7 +3,16 @@
 # select GPUs on the server
 export CUDA_VISIBLE_DEVICES="0"
 
-python tweak_utkface.py --model-name UTKFace_test --model-ckpt-name 0031 \
---adv-type eyeglasses --advatk-ckpt-root /tmp2/npfe/eyeglasses --advatk-stat-root /tmp2/npfe/eyeglasses_stats --advatk-name utkface_direct \
--b 128 --epochs 15 --lr 1e1 \
---fairness-matrix "equalized odds" --loss-type "perturb optim"\
+python tweak_utkface.py --model-name UTKFace_lr_2e_3_b1 --model-ckpt-name 0019 \
+--adv-type noise --advatk-ckpt-root /tmp2/npfe/noise --advatk-stat-root /tmp2/npfe/noise_stats \
+--advatk-name utkface_p_t --loss-type "perturb optim" --fairness-matrix "accuracy difference" \
+-b 256 --epochs 5 --lr 1e-1 \
+--coef-mode "static" \
+--coef 0.0 \
+
+python tweak_utkface.py --model-name UTKFace_lr_2e_3_b1 --model-ckpt-name 0019 \
+--adv-type noise --advatk-ckpt-root /tmp2/npfe/noise --advatk-stat-root /tmp2/npfe/noise_stats \
+--advatk-name utkface_pp_t --loss-type "perturb optim" --fairness-matrix "accuracy difference" \
+-b 256 --epochs 5 --lr 1e-1 \
+--coef-mode "dynamic" \
+--coef 0.1 \
--- tweak_utkfaceage.sh
+++ tweak_utkfaceage.sh
@@ -3,8 +3,20 @@
 # select GPUs on the server
 export CUDA_VISIBLE_DEVICES="0"
 
-python tweak_utkfaceage.py --model-name UTKFaceAge_test --model-ckpt-name 0031 \
+python tweak_utkfaceage.py --model-name UTKFaceAge_lr_5e_4_b1 --model-ckpt-name 0013 \
 --attr-list Age \
---adv-type eyeglasses --advatk-ckpt-root /tmp2/npfe/eyeglasses --advatk-stat-root /tmp2/npfe/eyeglasses_stats --advatk-name utkfaceage_poptim \
--b 128 --epochs 15 --lr 1e1 \
---fairness-matrix "equalized odds" --loss-type "perturb optim"\
+--adv-type eyeglasses --advatk-ckpt-root /tmp2/npfe/eyeglasses --advatk-stat-root /tmp2/npfe/eyeglasses_stats \
+--advatk-name utkfaceage_p_t --loss-type "perturb optim" --fairness-matrix "equalized odds" \
+-b 256 --epochs 5 --lr 1e1 \
+--coef-mode "static" \
+--p-coef 0.0 \
+--n-coef 0.0 \
+
+python tweak_utkfaceage.py --model-name UTKFaceAge_lr_5e_4_b1 --model-ckpt-name 0013 \
+--attr-list Age \
+--adv-type eyeglasses --advatk-ckpt-root /tmp2/npfe/eyeglasses --advatk-stat-root /tmp2/npfe/eyeglasses_stats \
+--advatk-name utkfaceage_pp_t --loss-type "perturb optim" --fairness-matrix "equalized odds" \
+-b 256 --epochs 5 --lr 1e1 \
+--coef-mode "dynamic" \
+--p-coef 0.05 \
+--n-coef 0.05 \
